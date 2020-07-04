d00f5f139c71b4f3c12bc2be1a3fda3c7976861b
Alin Olteanu <38540868+tetea90@users.noreply.github.com>
2020-07-04 17:26:27 +0300

    Create geometry_basics_2D.js

---
+++ 8-kyu/Geometry Basics: Distance between points in 2D/geometry_basics_2D.js
@@ -0,0 +1,14 @@
+/*
+This series of katas will introduce you to basics of doing geometry with computers.
+
+Point objects have x and y attributes (X and Y in C#) attributes.
+
+Write a function calculating distance between Point a and Point b.
+
+Tests round answers to 6 decimal places.
+*/
+
+//Solution:
+
+const distanceBetweenPoints = ({x:x1, y:y1}, {x:x2, y:y2}) =>
+  Math.hypot(x1 - x2, y1 - y2)
