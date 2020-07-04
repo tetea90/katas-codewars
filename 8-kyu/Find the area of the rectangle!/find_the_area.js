ab7f0e25ae67a412ddc6d588afc0f8ee6b61bf65
Alin Olteanu <38540868+tetea90@users.noreply.github.com>
2020-07-04 21:51:53 +0300

    Create find_the_area.js

---
+++ 8-kyu/Find the area of the rectangle!/find_the_area.js
@@ -0,0 +1,17 @@
+/*
+Find the area of a rectangle when provided with one diagonal and one side of the rectangle. If the input diagonal is less than or equal to the length of the side, return "Not a rectangle". If the resultant area has decimals round it to two places.
+
+This kata is meant for beginners. Rank and upvote to bring it out of beta!
+*/
+
+//Solution:
+
+function area(d,l){
+  if(d>l){
+    let num = l * (Math.sqrt(d**2 - l**2));
+    var n = num.toFixed(2)
+    return Number(n)
+  } else {
+    return "Not a rectangle"; 
+  }
+}
