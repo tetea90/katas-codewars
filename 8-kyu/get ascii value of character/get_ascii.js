480d927746d8e72f2911e4532a0af0ab82528d46
Alin Olteanu <38540868+tetea90@users.noreply.github.com>
2020-07-04 18:07:09 +0300

    Create get_ascii.js

---
+++ 8-kyu/get ascii value of character/get_ascii.js
@@ -0,0 +1,12 @@
+/*
+Get ASCII value of a character.
+
+For the ASCII table you can refer to http://www.asciitable.com/
+*/
+
+//Solution:
+
+function getASCII(c){
+  var code = c.charCodeAt();
+  return code
+}
